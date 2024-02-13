c9e62c6cdca0f2c59e89c848f9d5a7e88dcbde4b
Tausif64husain9410 <tausifhusain79@gmail.com>
2024-02-13 13:37:12 +0530

    variable and render and context from view to template index.html

--- home/views.py
+++ home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is home page")
+    context = {
+        'variable1': "this is sent",
+        'variable2': "you are great"
+    }
+    return render(request, 'index.html',context)
+    # return HttpResponse("this is home page")
 
 def about(request):
     return HttpResponse("this is about page")
