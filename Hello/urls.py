a32479aabb58f2ca603c9df7f7beb47562c68a13
Tausif64husain9410 <tausifhusain79@gmail.com>
2024-02-14 16:36:55 +0530

    new files create and made changes in some folders and file

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "My Ice Cream Admin"
+admin.site.site_title = "My Ice Cream Admin Portal"
+admin.site.index_title = "Welcome to My Ice Cream"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls'))
